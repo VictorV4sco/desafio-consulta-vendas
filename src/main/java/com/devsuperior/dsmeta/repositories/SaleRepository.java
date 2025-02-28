17a185a0660e54566f12b918da7019ce13e61ce3
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-02-28 12:19:12 -0300

    Tentando implementar a query

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,25 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+	@Query("SELECT obj FROM Sale obj " +
+		       "WHERE (:minDate IS NULL OR obj.date >= :minDate) " +
+		       "AND (:maxDate IS NULL OR obj.date <= :maxDate) " +
+		       "AND (:sellerName IS NULL OR LOWER(obj.seller.name) LIKE LOWER(CONCAT('%', :sellerName, '%')))")
+		Page<Sale> searchSales(
+		        @Param("minDate") LocalDate minDate,
+		        @Param("maxDate") LocalDate maxDate,
+		        @Param("sellerName") String sellerName,
+		        Pageable pageable);
 
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.entities.Seller;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 @Service
@@ -20,4 +24,9 @@ public class SaleService {
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+	
+//	public Page<SaleMinDTO> findByReport(Long id, LocalDate date, Double amount, Seller name, Pageable pageable) {
+//		Page<Sale> result = repository.searchByDate(date, pageable);
+//		return result.map(x -> new SaleMinDTO(x));
+//	}
 }
